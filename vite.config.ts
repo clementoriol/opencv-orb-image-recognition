d2c8c98995e00609a67fafa4dcbaaede66ff9dda
Clément Oriol <oriol.clement@gmail.com>
2025-06-02 09:26:49 +0200

    Add ssl to vite server + allow localtunnel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import type { UserConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
+  plugins: [basicSsl()],
   server: {
     port: 3000,
+    https: true, // enable HTTPS
+    allowedHosts: [".loca.lt"], // allow requests from localtunnel
   },
 } satisfies UserConfig;
